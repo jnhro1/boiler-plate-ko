b218fa3207ed4b3e6ba8cbc826afa7bc14a59eb1
jorang <jnhro1@gmail.com>
2020-09-12 17:24:54 +0900

    logout

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-
 const express = require('express')
 const app = express()
 const port = 5000
-
 const bodyParser = require('body-parser');
-const { User } = require("./models/User");
 const config = require('./config/key');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
+const { User } = require("./models/User");
+const { auth } = require("./middleware/auth");
 
 
 app.use(bodyParser.urlencoded({extended: true}));
-
 app.use(bodyParser.json());
 app.use(cookieParser());
 
@@ -23,7 +21,7 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req,res) => res.send("Hello World!~~~~~~~~~~~~"))
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
 
     //회원가입할때 필요한 정보들을 client에서 가져오면
     //그것들을 데이터베이스에 넣어준다.
@@ -38,7 +36,7 @@ app.post('/register', (req, res) => {
     })
 })
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
         
         //요청된 이메일을 데이터베이스에서 있는지 찾는다.
         User.findOne({ email: req.body.email}, (err, user) => {
@@ -67,5 +65,33 @@ app.post('/login', (req, res) => {
         })    
     })
 
+app.get('/api/users/auth', auth, (req, res) => {
+
+    //여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication 이 true 라는 말.
+    res.status(200).json({
+        _id: req.user._id,
+        // role 1 어드민 role 2 특정 부서 어드민
+        // role 0 -> 일반유저 role 0이 아니면 관리자
+        isAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+        
+    })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id},
+        {token: "" }
+        , (err, user) => {
+            if (err) return res.json({ success: false, err });
+            return res.status(200).send({
+                success: true
+            })
+        })
+})
 
 app.listen(port, () => console.log('example app listening on port ${port}!'))
