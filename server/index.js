a014a535a13006ef6213a5df17b292eb3a05d335
jorang <jnhro1@gmail.com>
2020-09-22 23:06:39 +0900

    update client

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 5000
 const bodyParser = require('body-parser');
 const config = require('./config/key');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { User } = require("./models/User");
 const { auth } = require("./middleware/auth");
+const port = 5000
 
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -20,6 +20,10 @@ mongoose.connect(config.mongoURI, {
   .catch(err => console.log(err))
 
 app.get('/', (req,res) => res.send("Hello World!~~~~~~~~~~~~"))
+
+app.get('/api/hello', (req, res) => {
+    res.send("안녕하세요 ~")
+})
 
 app.post('/api/users/register', (req, res) => {
 
